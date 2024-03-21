8d7a7566f4e50e381ba0bc82680571f269c62218
adrianlucenaa <alucvar0812@g.educaand.es>
2024-03-21 10:06:57 +0100

    ENDPOINT de clientes echo , a falta de algunos cambios

--- src/rutas/clientes.php
+++ src/rutas/clientes.php
@@ -5,25 +5,46 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../config/db.php';
 
- $app = AppFactory::create();
+$app = AppFactory::create();
+$app->setBasePath('/PruebaAPI/public');
 
- //Metodo get que se traiga a todos los clientes
- $app->get('/clientes', function (Request $request, Response $response) {
-    $Sql = "SELECT TOP * FROM Clientes";
-    try{
+// Metodo get que se traiga a todos los clientes
+$app->get('/clientes', function (Request $request, Response $response) {
+    $Sql = "SELECT TOP 1 * FROM Clientes";
+    try {
         $db = new db();
         $db = $db->connectDB();
         $resultado = $db->query($Sql);
-        if($resultado->rowCount() > 0){
+        if ($resultado->rowCount() > 0) {
             $clientes = $resultado->fetchAll(PDO::FETCH_OBJ);
-            echo json_encode($clientes);
-        }else{
-            echo json_encode("No existen clientes en la base datos");
+            $response->getBody()->write((string)json_encode($clientes));
+        } else {
+            $response->getBody()->write((string)json_encode("Ha llegado al else"));
         }
-    }catch(PDOException $e){
-        echo '{"error": {"text": '.$e->getMessage().'}';
+    } catch (PDOException $e) {
+        $response->getBody()->write((string)json_encode(['error' => $e->getMessage()]));
     }
+
+    return $response->withHeader('Content-Type', 'application/json');
 });
-
- $app->run();
-
+ /*
+$app->get('/clientes', function (Request $request, Response $response) {
+   
+    $Sql = "SELECT TOP 1 * FROM Clientes";
+    try {
+        $db = new db();
+        $db = $db->connectDB();
+        $resultado = $db->query($Sql);
+        if ($resultado->rowCount() > 0) {
+            $clientes = $resultado->fetchAll(PDO::FETCH_OBJ);
+            return $response->withJson($clientes);
+        } else {
+            return $response->withJson("No existen clientes en la base de datos");
+        }
+    } catch (PDOException $e) {
+        return $response->withStatus(500)->withJson(['error' => $e->getMessage()]);
+    }
+    
+});
+*/
+$app->run();
