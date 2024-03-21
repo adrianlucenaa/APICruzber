0ab4fcf37eac1e069f9373db7401c16f1f269b83
adrianlucenaa <alucvar0812@g.educaand.es>
2024-03-21 12:22:08 +0100

    Metodo Get funcionando y testeado

--- src/config/db.php
+++ src/config/db.php
@@ -1,4 +1,25 @@
 <?php
+/*
+    class db{
+
+        private $dbHost = 'localhost'; 
+        private $dbName = 'Cruzber';
+        private $dbUser = 'logic';
+        private $dbPass = 'Sage2009+';
+
+        public function connectDB(){
+        try {
+            $dbConnecion = new PDO("sqlsrv:server=$this->dbHost;database=$this->dbName", $this->dbUser, $this->dbPass);
+
+            echo ("Se conecta a la base de datos");
+            
+        }
+        catch (PDOException $e) {
+            echo ("No se conecta a la base de datos");
+            }
+        }
+    }   
+*/
     class db{
 
         private $dbHost = 'localhost'; 
@@ -9,10 +30,11 @@
         
         //coneccion
         public function connectDB(){
-            /* $dbConnecion = new PDO("sqlsrv:server=$this->dbHost;database=$this->dbName", $this->dbUser, $this->dbPass); */
+           // $dbConnecion = new PDO("sqlsrv:server=$this->dbHost;database=$this->dbName", $this->dbUser, $this->dbPass); 
             $dbConnecion = new PDO("sqlsrv:server=localhost;database=Cruzber", "logic", "Sage2009+");
             $dbConnecion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $dbConnecion;
         }
     }
 
+    ?>
