eac0219408900ac8e5e966f03aa1a255967bf13a
adrianlucenaa <alucvar0812@g.educaand.es>
2024-04-01 17:29:25 +0200

    Depuracion de codigo ,MVC añadido para la clase cliente

--- public/index.php
+++ public/index.php
@@ -9,18 +9,9 @@ require __DIR__ . '/../src/rutas/clientes.php';
 require __DIR__ . '/../src/config/db.php';
 
  $app = AppFactory::create();
- /*$app->setBasePath('/PruebaAPI/public'); */
 
-/*
- $app->get('/clientes', function (Request $request, Response $response) {
-    $conn = new PDO("sqlsrv:server=localhost;database=Cruzber","logic","Sage2009+");
-    $sql  = $conn->prepare("SELECT TOP 2 * FROM clientes");
-    $sql->execute();
-    $data = $sql->fetchAll(PDO::FETCH_ASSOC);
-    $response->getBody()->write(json_encode($data));
-    return $response;
-});
-*/
+
+
 
 //Lanzamos la App
 $app->run();
